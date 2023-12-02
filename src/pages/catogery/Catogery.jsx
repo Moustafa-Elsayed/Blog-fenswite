f3962670608482c0f0b6b75241f34dbc95597c68
Mostafa Elsayed <moelsayed949@gmail.com>
2023-12-02 21:42:55 +0200

    create catogery section

---
+++ src/pages/catogery/Catogery.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./catogery.scss";
+const Catogery = () => {
+  return (
+    <div className="customContainer">
+      <div className="catogery">
+        <div className="title">Choose A Catagory</div>
+        <div className="catogeryContainer">
+          <div className="catogeryCard">
+            <img src="./images/Icon.png" alt="" />
+            <div className="catogerytitle">Business</div>
+            <div className="catogeryHeader">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+            </div>
+          </div>
+          <div className="catogeryCard">
+            <img src="./images/Icon.png" alt="" />
+            <div className="catogerytitle">Business</div>
+            <div className="catogeryHeader">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+            </div>
+          </div>
+          <div className="catogeryCard">
+            <img src="./images/Icon.png" alt="" />
+            <div className="catogerytitle">Business</div>
+            <div className="catogeryHeader">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+            </div>
+          </div>
+          <div className="catogeryCard">
+            <img src="./images/Icon.png" alt="" />
+            <div className="catogerytitle">Business</div>
+            <div className="catogeryHeader">
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Catogery;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
+import Catogery from "../catogery/Catogery";
 import Hero from "../hero/Hero";
 import Mission from "../mission/Mission";
-import "./home.scss"
+import "./home.scss";
 
 const Home = () => {
   return (
     <div className="home">
       <Hero />
       <Mission />
+      <Catogery />
     </div>
   );
 };
