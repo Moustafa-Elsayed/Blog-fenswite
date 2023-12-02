37b098406aea8eb8fbf7211f2204a85f5c136698
Mostafa Elsayed <moelsayed949@gmail.com>
2023-12-02 21:17:39 +0200

    atyling featuredsection

--- src/pages/mission/Mission.jsx
+++ src/pages/mission/Mission.jsx
@@ -2,28 +2,34 @@ import "./mission.scss";
 const Mission = () => {
   return (
     <div className="customContainer">
-      <div className="mission">
-        <div className="firstSection">
-          <div className="firstTitle">ABOUT US</div>
-          <div className="secondTitle">
-            We are a community of content writers who share their learnings
+      <div className="lineContainer">
+        <div className="lineOne"></div>
+        <div className="lineTwo"></div>
+      </div>
+      <div className="mission-Container">
+        <div className="mission">
+          <div className="firstSection">
+            <div className="firstTitle">ABOUT US</div>
+            <div className="secondTitle">
+              We are a community of content writers who share their learnings
+            </div>
+            <div className="thirdTitle">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </div>
+            <div className="readMe">Read More ></div>
           </div>
-          <div className="thirdTitle">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </div>
-          <div className="readMe">Read More ></div>
-        </div>
-        <div className="secondSection">
-          <div className="firstHeader">Our mision</div>
-          <div className="secondheader">
-            Creating valuable content for creatives all around the world
-          </div>
-          <div className="thirdHeader">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
+          <div className="secondSection">
+            <div className="firstHeader">Our mision</div>
+            <div className="secondheader">
+              Creating valuable content for creatives all around the world
+            </div>
+            <div className="thirdHeader">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </div>
           </div>
         </div>
       </div>
